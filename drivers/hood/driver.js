72956db9ebc9ac3a1801c91acf3ba67e883a62c9
Emile Nijssen <emile@athom.nl>
2019-06-03 15:53:35 +0200

    Add Hood

--- drivers/hood/driver.js
+++ drivers/hood/driver.js
@@ -33,7 +33,7 @@ class HomeConnectDriverHood extends HomeConnectDriver {
 
 		new Homey.FlowCardAction('program_venting')
 			.register()
-			.registerRunListener( args => {
+			.registerRunListener(async args => {
 				const types = args.venting_level.split('_');
 
 				return args.device.startVenting(levelMap[types[0]], ventingLevelMap[args.venting_level]);
@@ -41,14 +41,14 @@ class HomeConnectDriverHood extends HomeConnectDriver {
 
 		new Homey.FlowCardAction('program_automatic')
 			.register()
-			.registerRunListener( args => {
-				return args.device.startProgram( programMap['automatic'])
+			.registerRunListener(async args => {
+				return args.device.startProgram(programMap['automatic']);
 			});
 
 		new Homey.FlowCardAction('program_delayed_shut_off')
 			.register()
-			.registerRunListener( args => {
-				return args.device.startProgram( programMap['delayed_shut_off'])
+			.registerRunListener(async args => {
+				return args.device.startProgram(programMap['delayed_shut_off']);
 			});
 	}
 
